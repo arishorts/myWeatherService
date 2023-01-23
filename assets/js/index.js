a2ed30251f4d0e8a79f9529b0553cfe5b86ed29d
arishorts <arieljschwartz@gmail.com>
2023-01-23 10:06:17 -0700

    Added getanylocation for the user to submit an input

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,10 +10,55 @@ const weatherApp = {
     //   .getElementById("btnAny")
     //   .addEventListener("click", weatherApp.getAnyLocation);
     weatherApp.getCurrentLocation();
-    weatherApp.fetchWeather();
+    weatherApp.getAnyLocation();
   },
-  fetchWeather: (ev) => {
-    //https://openweathermap.org/api/geocoding-api
+  fetchWeather: (lat, lon) => {
+    // //https://openweathermap.org/api/geocoding-api
+    // var city = "mesa";
+    // var state = "AZ";
+    var apikey = "443d0f967419d0d088b3f740ceaaae6e";
+    // var geourl = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},US&appid=${apikey}`;
+    // // var geoObj = {};
+    // // var geoarraylat = "";
+    // // var geoarraylon = "";
+    // fetch(geourl)
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     var geoObj = Object.assign(data);
+    //     var geoarraylat = geoObj[0].lat;
+    //     var geoarraylon = geoObj[0].lon;
+
+    //https://openweathermap.org/forecast5
+    //https://www.latlong.net/
+    // var lat = "33.415180";
+    // var lon = "-111.831497";
+    var weatherurl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apikey}`;
+    console.log(lat);
+    //https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+    //experienced a CORS error on fetch attemp 11:51PM 01/22/23
+    fetch(weatherurl)
+      .then((response) => response.json())
+      .then((data) => weatherApp.showWeather(data))
+      .catch((error) => {
+        console.log(error);
+        weatherApp.fail;
+      });
+    //https://www.youtube.com/watch?v=nGVoHEZojiQ&ab_channel=SteveGriffith-Prof3ssorSt3v3
+  },
+
+  getCurrentLocation: (ev) => {
+    let opts = {
+      enableHighAccuracy: true,
+      timeout: 1000 * 10, //seconds
+      maximumAge: 1000 * 60 * 5, //5 minutes
+    };
+    navigator.geolocation.getCurrentPosition(
+      weatherApp.successCurrent,
+      weatherApp.fail,
+      opts
+    );
+  },
+  getAnyLocation: (event) => {
     var city = "mesa";
     var state = "AZ";
     var apikey = "443d0f967419d0d088b3f740ceaaae6e";
@@ -27,45 +72,28 @@ const weatherApp = {
         var geoObj = Object.assign(data);
         var geoarraylat = geoObj[0].lat;
         var geoarraylon = geoObj[0].lon;
-
-        //https://openweathermap.org/forecast5
-        //https://www.latlong.net/
-        // var lat = "33.415180";
-        // var lon = "-111.831497";
-        var weatherurl = `https://api.openweathermap.org/data/2.5/forecast?lat=${geoarraylat}&lon=${geoarraylon}&appid=${apikey}`;
-
-        //https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-        //experienced a CORS error on fetch attemp 11:51PM 01/22/23
-        fetch(weatherurl)
-          .then((response) => response.json())
-          .then((data) => console.log(data));
-        //.then((data) => weatherApp.showWeather.data);
-        //https://www.youtube.com/watch?v=nGVoHEZojiQ&ab_channel=SteveGriffith-Prof3ssorSt3v3
+        weatherApp.successAny(geoarraylat, geoarraylon);
       });
   },
-  getCurrentLocation: (ev) => {
-    let opts = {
-      enableHighAccuracy: true,
-      timeout: 1000 * 10, //seconds
-      maximumAge: 1000 * 60 * 5, //5 minutes
-    };
-    navigator.geolocation.getCurrentPosition(
-      weatherApp.success,
-      weatherApp.fail,
-      opts
-    );
-  },
-  getAnyLocation: (ev) => {},
-  success: (position) => {
+  successCurrent: (position) => {
+    // show the longitude and latitude on the UI
     // document.getElementById("latitude").value =
     //   position.coords.latitude.toFixed(2);
     // document.getElementById("longitude").value =
     //   position.coords.longitude.toFixed(2);
   },
-  fail: (err) => {
-    console.log(err);
+  successAny: (lat, lon) => {
+    // show the longitude and latitude on the UI
+    // document.getElementById("latitude").value = lat;
+    // document.getElementById("longitude").value = lon;
+    weatherApp.fetchWeather(lat, lon);
   },
-  showWeather: (resp) => {},
+  fail: (err) => {
+    console.log("you have an error");
+  },
+  showWeather: (response) => {
+    console.log(response);
+  },
 };
 
 weatherApp.init();
